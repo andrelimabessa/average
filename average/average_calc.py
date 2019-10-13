2f741458ba11651e5f49e2eea857bbff47b9cb2a
Andre Jackson Gomes Bessa <andre.bessa@dataprev.gov.br>
2019-10-13 15:02:47 -0300

    Ajuste nome de variável

--- average/average_calc.py
+++ average/average_calc.py
@@ -4,23 +4,23 @@ from datetime import timedelta
 class AverageCalc:
 
     def __init__(self, window_size):
-        self.window_size = window_size
-        self.timestamp_duration =[]
-        self.delta_increment = timedelta(minutes=1)
-        self.delta_window = timedelta(minutes=10)
-        self.overlap_list = []
+        self.__window_size = window_size
+        self.__timestamp_duration =[]
+        self.__delta_increment = timedelta(minutes=1)
+        self.__delta_window = timedelta(minutes=10)
+        self.__overlap_list = []
     
     def add_tranlation_delivered(self, tranlation_delivered):   
-        button = self.__get_time_until_minutes(tranlation_delivered.timestamp) + self.delta_increment
-        top = button + self.delta_window
-        self.timestamp_duration.append((button, top, tranlation_delivered.duration))
+        button = self.__get_time_until_minutes(tranlation_delivered.timestamp) + self.__delta_increment
+        top = button + self.__delta_window
+        self.__timestamp_duration.append((button, top, tranlation_delivered.duration))
             
     def cal_avg_delivered_time(self):
 
         if len(self.timestamp_duration) > 0:
 
-            self.overlap_list = []
-            reference = self.__get_time_until_minutes(self.timestamp_duration[0][0]) - self.delta_increment
+            self.__overlap_list = []
+            reference = self.__get_time_until_minutes(self.__timestamp_duration[0][0]) - self.__delta_increment
             
             while len(self.timestamp_duration) > 0:
             
@@ -34,29 +34,29 @@ class AverageCalc:
         
         self.__remove_element_out_of_range(reference)
 
-        for item in self.timestamp_duration:
+        for item in self.__timestamp_duration:
             if item[0] > reference:
                 break
-            self.overlap_list.append(item)
+            self.__overlap_list.append(item)
     
     def __remove_element_out_of_range(self, reference):
         result = []
-        for item in self.overlap_list:
+        for item in self.__overlap_list:
             if item[1] > reference:
                 result.append(item)
-        self.overlap_list = result
+        self.__overlap_list = result
 
     def __update_timestamp_duration(self):
-        for item in self.overlap_list:
+        for item in self.__overlap_list:
             if item in self.timestamp_duration:
-                self.timestamp_duration.remove(item)
+                self.__timestamp_duration.remove(item)
 
     def __print_overlap_list(self, reference):
         avg = 0
         if len(self.overlap_list) > 0:
-            size = len(self.overlap_list)
+            size = len(self.__overlap_list)
             amount = 0
-            for item in self.overlap_list:
+            for item in self.__overlap_list:
                 amount += item[2]
             avg = amount/size
 
